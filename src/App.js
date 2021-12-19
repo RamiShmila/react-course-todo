db29b1a401d306ea4037ebfafca70f9cb401b357
rami shmila <shmilar@gmail.com>
2021-12-19 14:24:07 +0200

    end of the day

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import {Login} from './components/Login/Login.js';
 import './App.css';
 // import { Todo } from './components/Todo/Todo.js';
 import { Header } from './components/Header/Header.js';
+import { Todo } from './components/Todo/Todo.js';
+
 
 function App() {
   return (
+
     <div className="App">
         <Login></Login>
+        <Todo></Todo>
+     
+        
+        {/* <Todo></Todo>
+        
         <Header></Header>
         <Header firstName="Rami" lastName="Shmila"></Header>
         <Header firstName="Doron" lastName="Arnon"></Header>
-        <Header firstName="Igor" ></Header>
-        {/* <Todo></Todo>  */}
+        <Header firstName="Igor" ></Header> */}
+        
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,85 @@
-export function Login(){
+import { useState } from "react";
+export function Login() {
 
-const handleSubmit = (e) => {
-    console.log('rami');
-    e.preventDefault();
-}
+    // const handleSubmit = (e) => {
+    //     console.log('rami');
+    //     e.preventDefault();
+    // }
 
-return (
-    <form onSubmit={handleSubmit}>
-            Email:<input 
-                    type="email" 
-                    name="email" 
-                    placeholder="Enter your email">
-                    </input>
-            Passport:<input 
-                        type="password" 
-                        name="passport" 
-                        placeholder="Enter your passport">
-                    </input>
-                    <button type="submit" >Login</button>
+    const [user, setUser] = useState(null)
+    const [mail, setMail] = useState('yariv@nerdeez.com')
+    const [pass, setPass] = useState('12345678')
+
+    const handleMailChange = (e) => {
+        setMail(e.target.value);
+    }
+
+    const handlePassChange = (e) => {
+        setPass(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        // 
+        console.log('this will run when submitting the form');
+        e.preventDefault();
+
+
+
+        setUser({ firstName: 'rami1', lastName: 'sssss' })
+
+        const data = {
+            email: mail,
+            password: pass
+        };
+
+        fetch('https://academeez-login-ex.herokuapp.com/api/users/login', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                setUser(data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            Email:
+            <input
+                type="email"
+                name="email"
+                value={mail}
+                placeholder="Enter your email"
+                onChange={handleMailChange}
+            />
+                
+            
+            Passport:<input
+                type="password"
+                name="passport"
+                value={pass}
+                placeholder="Enter your passport"
+                onChange={handlePassChange}
+            />
+            <button type="submit">SendUserFromForm</button>
+
+            {/* hello rami you are now logged in  */}
+
+            {
+                user && <h1>Hello {user.firstName} you are now logged in, your id is {user.id}
+                    your createdAt is {user.createdAt} </h1>
+
+            }
+            {/* <button type="submit">GetUserFromServer</button> */}
         </form>
-)
+    )
 }
